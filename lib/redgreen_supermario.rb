daf191842aaa7c2167ddc95964c30cfdd95f41cd
Niklas Hofer <niklas+dev@lanpartei.de>
2011-06-17 17:12:48 +0200

    ⍾

--- lib/redgreen_supermario.rb
+++ lib/redgreen_supermario.rb
@@ -1,15 +1,23 @@
 module Color
+  SoundPlayer = '/usr/bin/afplay'
+
   class << self
     alias_method :orig_color, :color
-  end
-  def self.color(color)
-   Thread.new {
-      case color
-        when :green  then system("/usr/bin/afplay #{File.expand_path('../../sounds/smb_coin.wav', __FILE__)}")
-        when :yellow then system("/usr/bin/afplay #{File.expand_path('../../sounds/smb_bump.wav', __FILE__)}")
-        when :red    then system("/usr/bin/afplay #{File.expand_path('../../sounds/smb_bowserfalls.wav', __FILE__)}")
-      end
-    }
-    self.orig_color(color)
+
+    def color(color)
+     Thread.new {
+        case color
+          when :green  then play_sound('smb_coin')
+          when :yellow then play_sound('smb_bump')
+          when :red    then play_sound('smb_bowserfalls')
+        end
+      }
+      orig_color(color)
+    end
+
+    def play_sound(sound)
+      path = File.expand_path("../../sounds/#{sound}.wav", __FILE__)
+      system(SoundPlayer, path)
+    end
   end
 end
